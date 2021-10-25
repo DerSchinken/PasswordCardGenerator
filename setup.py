319705cc56c78fee7f0125ada782a5dae4ef248e
DerSchinken <paulhippler21@gmail.com>
2021-10-25 20:13:48 +0200

    fixed pyi and made raw to property; added mypy_cache to gitignore

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ with open("requirements.txt", "r") as requirements:
 
 setup(
     name="PasswordCardGenerator",
-    version="1.4.5",
+    version="1.4.6",
     # Major version 1
     # Minor version 4
-    # Maintenance version 5
+    # Maintenance version 6
 
     author="DerSchinken",
     maintainer="DerSchinken",
